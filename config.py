713033aa250af68678551200d5800629d47bd241
iamadao <43058757+iamadao@users.noreply.github.com>
2021-05-27 10:44:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ DEFAULT_HEADERS = {
 }
 
 
-COOKIE = ''
+COOKIE = '__ckguid=Snq5tsueIPqFtSByK1EYko4; __jsluid_s=dd294feed70455218b114d216084882f; device_id=1961150386162208333719018146cd9e419dc1d55a888d73b9bc579c4b; wt3_eid=%3B999768690672041%7C2161666793200215723%232161829436500631219; __jsluid_h=03b03e802618caa46ee4fed19ad84f1b; homepage_sug=a; r_sort_type=score; _zdmA.uid=ZDMA.kSwWkiTgp.1622083339.2419200; s_his=%E9%80%9A%E7%94%A8%20ro%E5%87%80%E6%B0%B4%E5%99%A8%2C510s%20mini%2Cnuc10i5%2Cnuc8i5beh; footer_floating_layer=0; ad_date=27; ss_ab=ss55; _zdmA.vid=*; bannerCounter=%5B%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%5D; ad_json_feed=%7B%7D; _zdmA.time=1622083375232.0.https%3A%2F%2Fwww.smzdm.com%2F; sess=ZjJlZjV8MTYyNzI2NzMzN3wyMTAxMjgzMDc4fDFjOTY3MzdiMTMyMzliZjUyNzE2ZDQ3OWQzYzAxOGM5fDE5NjExNTAzODYxNjIyMDgzMzM3MTkwMTgxNDZjZDllNDE5ZGMxZDU1YTg4OGQ3M2I5YmM1NzljNGJ8d2Vi; user=iamadao%7C2101283078; smzdm_id=2101283078'
 
 
 PUSH_PLUS_TOKEN = ''
